4666907dae95369788f5f1ba8bc6378f8c1ff23e
Cole <89951479+Zoot01@users.noreply.github.com>
2022-09-10 12:47:25 -0400

    feat: inital commit

---
+++ packages/backend/dist/server.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3001;
+app.get("/data", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    const data = { foo: "bar" };
+    res.json({ foo: "bar" });
+});
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
+});
